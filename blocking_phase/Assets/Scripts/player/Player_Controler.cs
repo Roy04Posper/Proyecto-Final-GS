e5e6f89500c63b0e0f72296e77826b398a250e86
Roy04Posper <Roypsda001@gmail.com>
2025-04-03 21:57:10 +0200

    Animaciones Personaje terminadas
    Animaciones del personaje principal concluidas, hay que arreglar la camara.

--- blocking_phase/Assets/Scripts/player/Player_Controler.cs
+++ blocking_phase/Assets/Scripts/player/Player_Controler.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("Horizontal Movement Settings:")]
     [SerializeField] private float walkSpeed = 1;
 
-    [SerializeField] private float jumpForce = 45;
+    [SerializeField] private float jumpForce = 45f;
 
     [Header("Ground Check Settings:")]
     [SerializeField] private Transform groundCheckPoint;
@@ -14,18 +14,36 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private float groundCheckX = 0.5f;
     [SerializeField] private LayerMask whatIsGround;
 
-    //References
-    Rigidbody2D rb;
-    private float xAxis;
-    private Animator anim;
 
+    private Rigidbody2D rb;
+    private float xAxis;
+    Animator anim;
+
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
 
+        anim = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         GetInputs();
@@ -51,11 +69,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
     }
 
-    void Move()
+    [System.Obsolete]
+    private void Move()
     {
-        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
-
-        anim.SetBool("Walking", rb.linearVelocity.x != 0 && Grounded());
+        rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+        anim.SetBool("Walking", rb.velocity.x != 0 && Grounded());
     }
 
     public bool Grounded()
@@ -72,20 +90,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
     }
 
+    [System.Obsolete]
     void Jump()
     {
-        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
         }
 
         if (Input.GetButtonDown("Jump") && Grounded())
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
         }
 
         anim.SetBool("Jumping", !Grounded());
-
     }
-
 }
